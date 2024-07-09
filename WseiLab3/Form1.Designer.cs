f12a4e2a39c5ddb82255f736dada15e44e35f3c5
yulia <yulia@YULLIA>
2024-07-09 15:17:13 +0200

    Add project files.

---
+++ WseiLab3/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace WseiLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.daneWejsciowe = new System.Windows.Forms.TextBox();
+            this.wynik = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dodawanie = new System.Windows.Forms.Button();
+            this.odejmowanie = new System.Windows.Forms.Button();
+            this.mnozenie = new System.Windows.Forms.Button();
+            this.dzielenie = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.rowna_sie = new System.Windows.Forms.Button();
+            this.wyczysc = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // daneWejsciowe
+            // 
+            this.daneWejsciowe.Location = new System.Drawing.Point(41, 43);
+            this.daneWejsciowe.Name = "daneWejsciowe";
+            this.daneWejsciowe.Size = new System.Drawing.Size(197, 26);
+            this.daneWejsciowe.TabIndex = 0;
+            // 
+            // wynik
+            // 
+            this.wynik.Location = new System.Drawing.Point(41, 309);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(197, 26);
+            this.wynik.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(145, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(103, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(36, 33);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(145, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(36, 33);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(61, 198);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(36, 33);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(103, 198);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(36, 33);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(145, 198);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(36, 33);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // dodawanie
+            // 
+            this.dodawanie.Location = new System.Drawing.Point(266, 67);
+            this.dodawanie.Name = "dodawanie";
+            this.dodawanie.Size = new System.Drawing.Size(36, 33);
+            this.dodawanie.TabIndex = 11;
+            this.dodawanie.Text = "+";
+            this.dodawanie.UseVisualStyleBackColor = true;
+            this.dodawanie.Click += new System.EventHandler(this.dodawanie_Click);
+            // 
+            // odejmowanie
+            // 
+            this.odejmowanie.Location = new System.Drawing.Point(266, 116);
+            this.odejmowanie.Name = "odejmowanie";
+            this.odejmowanie.Size = new System.Drawing.Size(36, 33);
+            this.odejmowanie.TabIndex = 12;
+            this.odejmowanie.Text = "-";
+            this.odejmowanie.UseVisualStyleBackColor = true;
+            this.odejmowanie.Click += new System.EventHandler(this.odejmowanie_Click);
+            // 
+            // mnozenie
+            // 
+            this.mnozenie.Location = new System.Drawing.Point(266, 168);
+            this.mnozenie.Name = "mnozenie";
+            this.mnozenie.Size = new System.Drawing.Size(36, 33);
+            this.mnozenie.TabIndex = 13;
+            this.mnozenie.Text = "*";
+            this.mnozenie.UseVisualStyleBackColor = true;
+            this.mnozenie.Click += new System.EventHandler(this.mnozenie_Click);
+            // 
+            // dzielenie
+            // 
+            this.dzielenie.Location = new System.Drawing.Point(266, 222);
+            this.dzielenie.Name = "dzielenie";
+            this.dzielenie.Size = new System.Drawing.Size(36, 33);
+            this.dzielenie.TabIndex = 14;
+            this.dzielenie.Text = "/";
+            this.dzielenie.UseVisualStyleBackColor = true;
+            this.dzielenie.Click += new System.EventHandler(this.dzielenie_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(103, 248);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(36, 33);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "0";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // rowna_sie
+            // 
+            this.rowna_sie.Location = new System.Drawing.Point(266, 275);
+            this.rowna_sie.Name = "rowna_sie";
+            this.rowna_sie.Size = new System.Drawing.Size(36, 33);
+            this.rowna_sie.TabIndex = 16;
+            this.rowna_sie.Text = "=";
+            this.rowna_sie.UseVisualStyleBackColor = true;
+            this.rowna_sie.Click += new System.EventHandler(this.rowna_sie_Click);
+            // 
+            // wyczysc
+            // 
+            this.wyczysc.Location = new System.Drawing.Point(145, 248);
+            this.wyczysc.Name = "wyczysc";
+            this.wyczysc.Size = new System.Drawing.Size(93, 33);
+            this.wyczysc.TabIndex = 17;
+            this.wyczysc.Text = "Wyczyść";
+            this.wyczysc.UseVisualStyleBackColor = true;
+            this.wyczysc.Click += new System.EventHandler(this.wyczysc_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(61, 248);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(36, 33);
+            this.button10.TabIndex = 18;
+            this.button10.Text = ",";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 395);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.wyczysc);
+            this.Controls.Add(this.rowna_sie);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.dzielenie);
+            this.Controls.Add(this.mnozenie);
+            this.Controls.Add(this.odejmowanie);
+            this.Controls.Add(this.dodawanie);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.daneWejsciowe);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox daneWejsciowe;
+        private System.Windows.Forms.TextBox wynik;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button dodawanie;
+        private System.Windows.Forms.Button odejmowanie;
+        private System.Windows.Forms.Button mnozenie;
+        private System.Windows.Forms.Button dzielenie;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button rowna_sie;
+        private System.Windows.Forms.Button wyczysc;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ WseiLab3/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WseiLab3
+{
+    public partial class Form1 : Form
+    {
+        double zmiennaWynik = 0;
+        string aktualnaOperacja = "";
+        bool nowaLiczba = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "1";
+            }
+            else
+                daneWejsciowe.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "2";
+            }
+            else
+                daneWejsciowe.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "3";
+            }
+            else
+                daneWejsciowe.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "4";
+            }
+            else
+                daneWejsciowe.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "5";
+            }
+            else
+                daneWejsciowe.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "6";
+            }
+            else
+                daneWejsciowe.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "7";
+            }
+            else
+                daneWejsciowe.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "8";
+            }
+            else
+                daneWejsciowe.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "9";
+            }
+            else
+                daneWejsciowe.Text += "9";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += "0";
+            }
+            else
+                daneWejsciowe.Text += "0";
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (nowaLiczba)
+                daneWejsciowe.Clear();
+
+            nowaLiczba = false;
+
+            if (daneWejsciowe.Text.Contains("+") || daneWejsciowe.Text.Contains("-") || daneWejsciowe.Text.Contains("*") || daneWejsciowe.Text.Contains("/"))
+            {
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text += ",";
+            }
+            else
+                daneWejsciowe.Text += ",";
+        }
+
+        private void dodawanie_Click(object sender, EventArgs e)
+        {
+            if (zmiennaWynik != 0)
+            {
+                rowna_sie.PerformClick();
+                aktualnaOperacja = "+";
+                zmiennaWynik = Double.Parse(wynik.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = dodawanie.Text;
+            }
+            else
+            {
+                aktualnaOperacja = "+";
+                zmiennaWynik = Double.Parse(daneWejsciowe.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = dodawanie.Text;
+            }
+        }
+
+        private void odejmowanie_Click(object sender, EventArgs e)
+        {
+            if (zmiennaWynik != 0)
+            {
+                rowna_sie.PerformClick();
+                aktualnaOperacja = "-";
+                zmiennaWynik = Double.Parse(wynik.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = odejmowanie.Text;
+            }
+            else
+            {
+                aktualnaOperacja = "-";
+                zmiennaWynik = Double.Parse(daneWejsciowe.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = odejmowanie.Text;
+            }
+        }
+
+        private void mnozenie_Click(object sender, EventArgs e)
+        {
+            if (zmiennaWynik != 0)
+            {
+                rowna_sie.PerformClick();
+                aktualnaOperacja = "*";
+                zmiennaWynik = Double.Parse(wynik.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = mnozenie.Text;
+            }
+            else
+            {
+                aktualnaOperacja = "*";
+                zmiennaWynik = Double.Parse(daneWejsciowe.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = mnozenie.Text;
+            }
+        }
+
+        private void dzielenie_Click(object sender, EventArgs e)
+        {
+            if (zmiennaWynik != 0)
+            {
+                rowna_sie.PerformClick();
+                aktualnaOperacja = "/";
+                zmiennaWynik = Double.Parse(wynik.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = dzielenie.Text;
+            }
+            else
+            {
+                aktualnaOperacja = "/";
+                zmiennaWynik = Double.Parse(daneWejsciowe.Text);
+                nowaLiczba = true;
+                daneWejsciowe.Clear();
+                daneWejsciowe.Text = dzielenie.Text;
+            }
+        }
+
+
+        private void rowna_sie_Click(object sender, EventArgs e)
+        {
+            switch (aktualnaOperacja)
+            {
+                case "+":
+                zmiennaWynik += Double.Parse(daneWejsciowe.Text);
+                break;
+            case "-":
+                zmiennaWynik -= Double.Parse(daneWejsciowe.Text);
+                break;
+            case "*":
+                zmiennaWynik *= Double.Parse(daneWejsciowe.Text);
+                break;
+            case "/":
+                if (daneWejsciowe.Text != "0")
+                {
+                    zmiennaWynik /= Double.Parse(daneWejsciowe.Text);
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Nie można dzielić przez zero!");
+                    break;
+                }
+
+            }
+
+            wynik.Text = zmiennaWynik.ToString();
+            nowaLiczba = true;
+            daneWejsciowe.Clear();
+        }
+
+        private void wyczysc_Click(object sender, EventArgs e)
+        {
+            daneWejsciowe.Clear();
+            zmiennaWynik = 0;
+            wynik.Text = "0";
+            nowaLiczba = true;
+        }
+    }
+}
+
